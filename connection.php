d2053f4160bfa8d0c950ac6b84f00f2f517eded2
Bufford Brian Sta Maria <iambufford99@gmail.com>
2020-04-04 12:14:51 -0400

    feat(login): add backend

--- connection.php
+++ connection.php
@@ -11,9 +11,8 @@
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
     } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        echo "Connection failed: " . $e->getMessage() + "\n";
     }
 
 ?>
--- login.php
+++ login.php
@@ -2,4 +2,41 @@
 
 require_once('connection.php');
 
+// global constants
+$login_error_msg = 'Invalid username and/or password';
+
+// get req params from POST request
+$uname = !empty($_POST['uname']) ? trim($_POST['uname']) : null;
+$psw = !empty($_POST['psw']) ? trim($_POST['psw']) : null;
+
+// construct SQL statement and prepare it
+$sql = "SELECT id, username, pass FROM Account WHERE username = :username;";
+$stmt = $conn->prepare($sql);
+
+// bind value for prepare statement
+$stmt->bindValue(':username', $uname);
+
+// execute statement
+$stmt->execute();
+
+// fetch row
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($row === false) {
+    die($login_error_msg);
+} else {
+    $validPassword = password_verify($psw, $row['pass']);
+
+    if ($validPassword) {
+        $_SESSION['account_id'] = $row['id'];
+        $_SESSION['logged_in'] = time();
+
+        // redirect to homepage
+        header('Location: home.html');
+        exit;
+    } else {
+        die($login_error_msg);
+    }
+}
+
 ?>
