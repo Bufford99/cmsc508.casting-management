317aec80419dee801ca46406e36ea7a916356609
Bufford Brian Sta Maria <iambufford99@gmail.com>
2020-04-12 20:23:28 -0400

    change sign up

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function displaySignUp() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('signUpForm').style.display = 'block';
+}
+
+function displayLogin() {
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('signUpForm').style.display = 'none';
+}
