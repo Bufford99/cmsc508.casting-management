59e882998cd04eccbc2c61883a985c1fd9591872
Bufford99 <iambufford99@gmail.com>
2020-05-03 22:36:47 -0400

    display applications and favorites

--- index.php
+++ index.php
@@ -2,6 +2,86 @@
 
 session_start();
 
+require_once('connection.php');
+
+function getApplications() {
+    global $conn;
+
+    if(isset($_SESSION['user'])) {
+        $application_id = $_SESSION['user'];
+
+        $sql = "SELECT mc.name AS 'movie_character', mv.title AS 'movie_title', a.status AS 'status'
+            FROM Application a JOIN JobPosting j ON a.job_posting = j.id
+            JOIN MovieCharacter mc ON j.movie_character = mc.id
+            JOIN Movie mv ON mc.movie = mv.id
+            WHERE a.owner = :aid";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':aid', $application_id);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($stmt->rowCount() > 0) {
+            echo '<div>';
+            echo '<table style="width: 40%">';
+            echo '<tr>';
+            echo '<th align="left">Character</th>';
+            echo '<th align="left">Movie</th>';
+            echo '<th align="left">Status</th>';
+
+            while ($row) {
+                echo '<tr>';
+                echo '<td>'.$row['movie_character'].'</td>';
+                echo '<td>'.$row['movie_title'].'</td>';
+                echo '<td>'.$row['status'].'</td>';
+                echo '</tr>';
+
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            echo '</table>';
+            echo '</div>';
+        }
+    }
+}
+
+function getFavorites() {
+    global $conn;
+
+    if(isset($_SESSION['user'])) {
+        $application_id = $_SESSION['user'];
+
+        $sql = "SELECT mc.name AS 'movie_character', mv.title AS 'movie_title'
+            FROM Favorite f JOIN JobPosting j ON f.job_posting = j.id
+            JOIN MovieCharacter mc ON j.movie_character = mc.id
+            JOIN Movie mv ON mc.movie = mv.id
+            WHERE f.owner = :aid";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':aid', $application_id);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($stmt->rowCount() > 0) {
+            echo '<div>';
+            echo '<table style="width: 40%; position: absolute; top: 200px;">';
+            echo '<tr>';
+            echo '<th align="left">Character</th>';
+            echo '<th align="left">Movie</th>';
+
+            while ($row) {
+                echo '<tr>';
+                echo '<td>'.$row['movie_character'].'</td>';
+                echo '<td>'.$row['movie_title'].'</td>';
+                echo '</tr>';
+
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+            echo '</table>';
+            echo '</div>';
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +95,17 @@ session_start();
 
     <link rel="stylesheet" href="css/styles.css" />
     <link rel="stylesheet" href="css/index.css" />
+
+    <style>
+        table {
+            position: absolute;
+        }
+        table, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            background-color: #fff;
+        }
+    </style>
 
     <title>Casting Management</title>
 </head>
@@ -43,6 +134,8 @@ session_start();
     </header>
     <section id="showcase">
         <div class="container">
+            <?php getApplications() ?>
+            <?php getFavorites() ?>
             <form>
                 <div class="box">
                     <button id="searchButton">Find my next role!</button>
