1af5587cdad2afb3da75cd5b494a2c9325c52ee0
Bufford99 <iambufford99@gmail.com>
2020-05-06 10:02:56 -0400

    fix posting

--- manager/posting.php
+++ manager/posting.php
@@ -28,7 +28,7 @@ if (isset($_GET['action']) == 'create-posting') {
     $sql = "INSERT INTO JobPosting(id, manager, location, movie_character) VALUES(:id, :manager, :loc, :mc)";
     $stmt = $conn->prepare($sql);
     $stmt->bindValue(':id', $movieCharacter);
-    $stmt->bindValue(':manager', $_SESSION['manager']);
+    $stmt->bindValue(':manager', $_SESSION['pmanager']);
     $stmt->bindValue(':loc', $location);
     $stmt->bindValue(':mc', $movieCharacter);
     $stmt->execute();
